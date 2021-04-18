b0523753c1cb91a31c27f6038a170dad87b2a3a4
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-04-18 21:22:51 +0300

    18 April 2021

--- main.js
+++ main.js
@@ -4,24 +4,30 @@
 --Каждому контакту добавить кнопку для удаления контакта.
 --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
 */
-//const form01=document.getElementById("form01")
 
+
+const btSave=document.getElementById("btsave")
+function saveResponses() {
+//btSave.onclick =()=> {
+//for(let i=0; i<localStorage.length; i++) {
+//    let key = localStorage.key(i);
+//    alert(`${key}: ${localStorage.getItem(key)}`);
+    let usersObj = {
+        name: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        lastname2:document.getElementById("lastname2").value,
+        phonenumber: document.getElementById("phonenumber").value,
+        email:document.getElementById("email").value,
+        date:document.getElementById("date").value,
+    }
+    let key=document.getElementById("recordnumber").value;
+    localStorage.setItem(key,JSON.stringify(usersObj));
+}
+btSave.addEventListener("click", saveResponses);
 
 
 const btErase=document.getElementById("bterase")
 btErase.onclick =()=>{
-    localStorage.clear()
-}
-
-const btSave=document.getElementById("btsave")
-btSave.onclick =()=> {
-    let usersObj = {
-        firstname: document.getElementById("firstname").value,
-        lastname: document.getElementById("lastname").value,
-        lastname2: document.getElementById("lastname2").value,
-        phonenumber: document.getElementById("phonenumber").value,
-        email: document.getElementById("email").value,
-        date: document.getElementById("date").value
-    }
-    localStorage.setItem("1",JSON.stringify(usersObj));
+    let key=document.getElementById("recordnumber").value;
+    localStorage.removeItem(key);
 }
