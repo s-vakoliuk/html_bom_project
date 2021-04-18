a49b2f89e91b733c7a47665f523b1600ec88408d
SerhiiVakoliuk <79380482+s-vakoliuk@users.noreply.github.com>
2021-04-19 00:39:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,12 @@
 --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
 */
 
-
 const btSave=document.getElementById("btsave")
-function saveResponses() {
-
-    let usersObj = {
+btSave.onclick =()=> {
+let usersObj = {
         name: document.getElementById("firstname").value,
         lastname: document.getElementById("lastname").value,
-        lastname2:document.getElementById("lastname2").value,
+        lastname2: document.getElementById("lastname2").value,
         phonenumber: document.getElementById("phonenumber").value,
         email:document.getElementById("email").value,
         date:document.getElementById("date").value,
@@ -20,8 +18,23 @@ function saveResponses() {
     let key=document.getElementById("recordnumber").value;
     localStorage.setItem(key,JSON.stringify(usersObj));
 }
-btSave.addEventListener("click", saveResponses);
 
+const btEdit=document.getElementById("btedit")
+btEdit.onclick =()=> {
+       let usersObj = {
+        name: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        lastname2: document.getElementById("lastname2").value,
+        phonenumber: document.getElementById("phonenumber").value,
+        email:document.getElementById("email").value,
+        date:document.getElementById("date").value,
+    }
+    let usersObjEdit ={};
+    let key= document.getElementById("recordnumber").value;
+    usersObjEdit =JSON.parse(JSON.stringify(usersObj));
+    localStorage.getItem(key);
+    console.log(usersObjEdit);
+}
 
 const btErase=document.getElementById("bterase")
 btErase.onclick =()=>{
